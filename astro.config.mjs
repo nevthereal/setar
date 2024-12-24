24c55ebc92f4a88e128a9fec6b0a84df08095991
Neville Brem <bremneville@gmail.com>
2024-12-24 11:49:59 +0100

    reset code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel';
 
-import svelte from "@astrojs/svelte";
+import svelte from '@astrojs/svelte';
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  adapter: vercel(),
+  integrations: [svelte(), tailwind()]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-  },
-  daisyui: {
-    // Add your daisy ui themes here
-    themes: ["business"],
-  },
-  plugins: [require("daisyui"), require("@tailwindcss/typography")],
-};
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
